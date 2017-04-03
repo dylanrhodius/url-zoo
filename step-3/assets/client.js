6cf891f51fd541efb245a09fe0fa43a381a35a0d
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-04-03 15:26:46 +0100

    Rename text to generatedUrl

--- step-3/assets/client.js
+++ step-3/assets/client.js
@@ -18,13 +18,13 @@ function UrlList(props) {
 
 function genShortURL()
 {
-    var text = "";
+    var generatedUrl = "";
     var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
     for( var i=0; i < 5; i++ )
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+        generatedUrl += possible.charAt(Math.floor(Math.random() * possible.length));
 
-    return text;
+    return generatedUrl;
 }
 
 
