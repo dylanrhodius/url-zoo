b5e604219c49cded06bfc492a5a063a61e054325
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-04-06 00:23:17 +0100

    Create verbs and zoo files

---
+++ step-3/src/verbs.js
@@ -0,0 +1,13 @@
+const VERBS =
+   [
+   'Abiding',	'Arising',	'Awaking',	'Befalling',	'Bending',	'Betting',	'Bidding',	'Binding',	'Biting',	'Bleeding',	'Blowing',	'Breeding',	'Building',	'Burning',	'Bursting',	'Buying',	'Catching',	'Clinging',	'Creeping',	'Cutting',
+   'Daring',	'Digging',	'Diving',	'Drawing',	'Dreaming',	'Drinking',	'Driving',	'Dwelling',	'Eating',	'Falling',	'Feeding',	'Fighting',	'Fleeing',	'Flinging',	'Flying',	'Forbidding',	'Foregetting',	'Foreseeing',	'Foretelling',	'Forgiving',
+   'Forsaking',	'Freezing',	'Giving',	'Grinding',	'Growing',	'Hanging',	'Hearing',	'Hiding',	'Hitting',	'Holding',	'Hurting',	'Kneeling',	'Knitting',	'Knowing',	'laying',	'Leading',	'Leaning',	'Leaping',	'Learning',	'Leaving',
+   'Lending',	'Letting',	'Losing',	'Lying',	'Meeting',	'Melting',	'Misgiving',	'Misleading',	'Misreading',	'Overeating',	'Overhearing',	'Overleaping',	'Overseeing',	'Oversleeping',	'Overtaking',	'Overthrowing',	'Partaking',	'Paying',	'Pleading',	'Presetting',
+   'Quitting',	'Reading',	'Rebuilding',	'Recasting',	'Relaying',	'Renting',	'Repaying',	'Rerunning',	'Resetting',	'Retelling',	'Rethinking',	'Rewinding',	'Riding',	'Ringing',	'Rising',	'Riving',	'Running',	'Sawing',	'Saying',	'Seeing',
+   'Seeking',	'Selling',	'Sending',	'Setting',	'Sewing',	'Shaking',	'Shaving',	'Shedding',	'Shining',	'Shooting',	'Showing',	'Shrinking',	'Singing',	'Sinking',	'Sitting',	'Slaying',	'Sleeping',	'Sliding',	'Slinging',	'Slinking',
+   'Slitting',	'Smelling',	'Smiting',	'Sowing',	'Speaking',	'Speeding',	'Spending',	'Spilling',	'Spinning',	'Spitting',	'Splitting',	'Spoiling',	'Spoonfeeding',	'Spreading',	'Standing',	'Stealing',	'Sticking',	'Stinging',	'Stinking',	'Striking',
+   'Striving',	'Sunburning',	'Swearing',	'Sweeping',	'Swelling',	'Swimming',	'Swinging',	'Teaching',	'Tearing',	'Telling',	'Thinking',	'Thriving',	'Throwing',	'Thrusting',	'Treading',	'Unbending',	'Unbinding',	'Underlying',	'Understanding',	'Undoing',
+   'Unwinding',	'Upholding',	'Upsetting',	'Waking',	'Weaving',	'Wedding',	'Weeping',	'Wetting',	'Winding',	'Winning',	'Writing'
+   ]
+module.exports = VERBS
